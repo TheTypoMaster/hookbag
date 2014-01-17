9ab433ba67d6c1904e0a87b2e6bd20ba5c21f9ca
liujjxi <liujjxi@qq.com>
2014-01-17 14:50:48 +0800

    header affix top

--- page/union/js/scrollview.js
+++ page/union/js/scrollview.js
@@ -1,48 +1,44 @@
 $(function() {
   var $blocks = $('.animated.notViewed');
   var $window = $(window);
-
-  $window.on('scroll', function(e){
-    $blocks.each(function(i,elem){
+	function checkElem(){
+    $blocks.each(function(){
       if($(this).hasClass('viewed')) 
         return;
         
       isScrolledIntoView($(this));
     });
-  });
+  }
+  $window.on('scroll',checkElem );
 });
 function isScrolledIntoView(elem) {
-  $(window).scroll(function(){
-		var docViewTop = $(window).scrollTop();
-		var docViewBottom = docViewTop + $(window).height()*1.5;
-		var elemOffset = 0;
-		
-		if(elem.data('offset') != undefined) {
-			elemOffset = elem.data('offset');
-		}
-		var elemTop = $(elem).offset().top;
-		var elemBottom = elemTop + $(elem).height();
-		
-		if(elemOffset != 0) { // custom offset is updated based on scrolling direction
-			if(docViewTop - elemTop >= 0) {
-				// scrolling up from bottom
-				elemTop = $(elem).offset().top + elemOffset;
-			} else {
-				// scrolling down from top
-				elemBottom = elemTop + $(elem).height() - elemOffset
-			}
-		}
-		if((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
-			// once an element is visible exchange the classes
-			$(elem).removeClass('notViewed').addClass('viewed');
-			
-			var animElemsLeft = $('.animated.notViewed').length;
-			if(animElemsLeft == 0){
-				// with no animated elements left debind the scroll event
-				$(window).off('scroll');
-			}
-		}else if(elemBottom > docViewBottom){
-			$(elem).removeClass('viewed').addClass('notViewed');
-		}
-	});
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height()*1.5;
+  var elemOffset = 0;
+  
+  if(elem.data('offset') != undefined) {
+    elemOffset = elem.data('offset');
+  }
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+  
+  if(elemOffset != 0) { // custom offset is updated based on scrolling direction
+    if(docViewTop - elemTop >= 0) {
+      // scrolling up from bottom
+      elemTop = $(elem).offset().top + elemOffset;
+    } else {
+      // scrolling down from top
+      elemBottom = elemTop + $(elem).height() - elemOffset
+    }
+  }
+  if((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+    // once an element is visible exchange the classes
+    $(elem).removeClass('notViewed').addClass('viewed');
+    
+    var animElemsLeft = $('.animated.notViewed').length;
+    if(animElemsLeft == 0){
+      // with no animated elements left debind the scroll event
+      $(window).off('scroll');
+    }
+  }
 }
