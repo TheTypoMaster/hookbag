85e86cca4b06251f0aa7a27805389e3bd890fa93
zvg <zzgvvg@gmail.com>
2014-01-19 10:35:26 +0800

    update

--- page/union/js/scrollview.js
+++ page/union/js/scrollview.js
@@ -1,6 +1,7 @@
 $(function() {
-  var $blocks = $('.animated.notViewed');
+  var $blocks = $('.animated');
   var $window = $(window);
+	checkElem();
 	function checkElem(){
     $blocks.each(function(){
       if($(this).hasClass('viewed')) 
@@ -13,28 +14,15 @@ $(function() {
 });
 function isScrolledIntoView(elem) {
   var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height()*1.5;
-  var elemOffset = 0;
+  var docViewBottom = docViewTop + $(window).height();
   
-  if(elem.data('offset') != undefined) {
-    elemOffset = elem.data('offset');
-  }
   var elemTop = $(elem).offset().top;
   var elemBottom = elemTop + $(elem).height();
   
-  if(elemOffset != 0) { // custom offset is updated based on scrolling direction
-    if(docViewTop - elemTop >= 0) {
-      // scrolling up from bottom
-      elemTop = $(elem).offset().top + elemOffset;
-    } else {
-      // scrolling down from top
-      elemBottom = elemTop + $(elem).height() - elemOffset
-    }
-  }
-  if((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+  if((elemTop <= docViewBottom) && (elemTop >= docViewTop)) {
     // once an element is visible exchange the classes
     $(elem).removeClass('notViewed').addClass('viewed');
-    
+    console.log(elemTop+'-'+elemBottom);
     var animElemsLeft = $('.animated.notViewed').length;
     if(animElemsLeft == 0){
       // with no animated elements left debind the scroll event
