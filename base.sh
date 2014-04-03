107ab65869e840a2f066c2bd6da37ed970b955a7
zvg <zzgvvg@gmail.com>
2014-04-03 11:12:45 +0800

    holder

--- base.sh
+++ base.sh
@@ -8,5 +8,5 @@ mkdir -p ./style/js
 mkdir -p ./style/img
 mkdir -p ./style/fonts
 rsync --progress -a --delete -e "ssh -q" ~/Sites/base/style/base/sui/* ./style/
-rsync --progress -a --delete -e "ssh -q" ~/Sites/base/style/base/holderjs/* ./_temp/js/
+rsync --progress -a --delete -e "ssh -q" ~/Sites/base/style/base/holderjs/* ./js/
 rsync --progress -a --delete -e "ssh -q" ~/Sites/base/style/base/hint/hint.css ./_temp/css/
